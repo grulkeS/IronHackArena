5a08788cd3c05179e5385cad5096f8f7de5b4fc3
stefan grulke <stefan.grulke@gmx.de>
2019-09-13 01:26:56 +0200

    reworked styling method to be more flexible

--- js/IronHackArena.js
+++ js/IronHackArena.js
@@ -29,7 +29,7 @@ class IronHackGame {
       "Superman flies towards the enemie and throws a punch. Wow what a punch! Deals 25 damage to its target.",
       "Superman uses his laserbeams from his eyes, leaving initial 15 damage and the target burns for 2 turns, 5 damage each turn!",
       "Superman blows his superhuman breath on every enemy! Deals 10 damage to all enemies.",
-      "Superman flies to the sun and becomes invulnerable for one turn, removes all debuffs!",      
+      "Superman flies to the sun and becomes invulnerable for one turn, removes all debuffs!",
       "Batman punches with the force of all his anger and desperation! Deals 20 damage to its target.",
       "Batman throws his Batwing and stuns for one turn!",
       "Batman calls Robin, together they punch even harder! Deals 30 damage to its target.",
@@ -51,6 +51,33 @@ class IronHackGame {
       "Ironman, also known as Tony Stark, is a genius engineer fighting with futuretech armored Suits and gets all the ladies!",
       "Superman, disguises himself as human in the form of Clark Kent, he comes from another planet and has superhuman skills thanks to our yellow Sun!"
     ]
+
+    this.idArray = [
+      [0, 0, 0],
+      [0, 0, 1],
+      [0, 0, 2],
+      [0, 0, 3],
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 1, 2],
+      [0, 1, 3],
+      [0, 2, 0],
+      [0, 2, 1],
+      [0, 2, 2],
+      [0, 2, 3],
+      [1, 0, 0],
+      [1, 0, 1],
+      [1, 0, 2],
+      [1, 0, 3],
+      [1, 1, 0],
+      [1, 1, 1],
+      [1, 1, 2],
+      [1, 1, 3],
+      [1, 2, 0],
+      [1, 2, 1],
+      [1, 2, 2],
+      [1, 2, 3]
+    ]
     this.skillNumbers = document.getElementsByClassName("skills");
     this.avatarTarget = document.getElementsByClassName("avatar");
     this.skillsp0 = document.getElementsByClassName("p0skills");
@@ -67,13 +94,13 @@ class IronHackGame {
     if (this.state === "start") {
       document.getElementsByClassName("gameArea")[0].style.opacity = 0.5;
     } else if (this.state === "choosingSkills") {
-      this.selectedSkillsStyling();
+      this.newStylingFrameWork();
     } else if (this.state === "skillChosen") {
-      this.selectedSkillsStyling();
+      this.newStylingFrameWork();
     }
     else if (this.state === "avatarChosen") {
       //add sourceAvatar+SourceSkill zu Target AvatarBox
-      this.selectedSkillsStyling();
+      this.newStylingFrameWork();
     }
   }
 
@@ -91,32 +118,47 @@ class IronHackGame {
   /*
   Array.from(document.getElementsByClassName("events")).forEach(function(item) {    console.log(item.id); });
   list = document.getElementsByClassName("events"); for (let item of list) {     console.log(item.id); }
+    chars&skills help:
+    this.charName = name;
+    this.health = 100;
+    this.chosenSkill = "";
+    this.isActive = 0;
+    this.beeingAttacked = [];
+    this.beeingBuffed = [];
+    this.isInvulnerable = 0;
+    this.playerID = "";
+    this.skills = [
+      { type: "damage", value: 10, rounds: 1, viableTarget: "opponent", coolDown: 1, isAvailable: 0 },
+      { type: "heal", value: 10, rounds: 1, viableTarget: "opponent", coolDown: 1, isAvailable: 0 },
+      { type: "dot", value: 5, rounds: 2, viableTarget: "allied", coolDown: 2, isAvailable: 0 },
+      { type: "invulnerable", value: 1, rounds: 1, viableTarget: "self", coolDown: 3, isAvailable: 0 }
+    ];
   */
-  selectedSkillsStyling() {
-    //checks for stage and reacts on it with styling changes in regards to selection
+  newStylingFrameWork() {
     switch (this.state) {
+
       case "choosingSkills":
         document.getElementsByClassName("gameArea")[0].style.opacity = 1;
-        for (let a = 0; a <= this.avatarTarget.length - 1; a++) {
-          this.avatarTarget[a].style.opacity = 0.8;
-        }
-        /*this.avatarTarget.forEach((avatarImg) => {
-          avatarImg.style.opacity = 0.8;
-        });*/
-        for (let i = 0; i <= this.skillNumbers.length - 1; i++) {
-          this.skillNumbers[i].style.opacity = 1;
-        }
-        /*this.skillNumbers.forEach((skillImg) => {
-        skillImg.style.opacity = 1;
-        });*/
         if (this.turn === 0) {
-          for (let i = 0; i <= this.skillsp1.length - 1; i++) {
-            document.getElementsByClassName("p1skills")[i].style.opacity = 0.8;
-          }
-        } else {
-          for (let i = 0; i <= this.skillsp0.length - 1; i++) {
-            document.getElementsByClassName("p0skills")[i].style.opacity = 0.8;
-          }
+          this.idArray.forEach((coord) => {
+            if (coord[0] === 0) {
+              if (this.players[0].characters[coord[1]].skills[coord[2]].isAvailable === 0 && this.players[0].characters[coord[1]].chosenSkill === ""/*&& checkResources()=== true*/) {
+                document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode.style.opacity = 1;
+              } else {
+                document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode.style.opacity = 0.7;
+              }
+            }
+          })
+        } else if (this.turn === 1) {
+          this.idArray.forEach((coord) => {
+            console.log(coord);
+            if (coord[0] === 1) {
+              if (this.players[1].characters[coord[1]].skills[coord[2]].isAvailable === 0 /*&& checkResources()=== true*/) {
+                console.log(document.getElementById("" + coord[0] + coord[1] + coord[2]));
+                document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode.style.opacity = 1;
+              }
+            }
+          })
         }
         break;
 
@@ -124,50 +166,181 @@ class IronHackGame {
         for (let s = 0; s <= this.skillNumbers.length - 1; s++) {
           this.skillNumbers[s].style.opacity = 0.8;
         }
-        /*this.skillNumbers.forEach((skillImg) => {
-        skillImg.style.opacity = 0.8;
-        });*/
         for (let i = 0; i <= this.players.length - 1; i++) {
           for (let j = 0; j <= this.players[i].characters.length - 1; j++) {
             if (this.players[i].characters[j].chosenSkill !== "") {
               document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).parentNode.style.opacity = 1;
               document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).border = "1";
-              //document.getElementById("selectedSkill").src="";
+              console.log(document.getElementById("selectedSkill"));
             }
           }
         }
-        if (this.turn === 0) {
-          for (let a = 0; a <= this.avatarp1.length - 1; a++) {
-            this.avatarp1[a].style.opacity = 1;
-          }
-        } else {
-          for (let a = 0; a <= this.avatarp0.length - 1; a++) {
-            this.avatarp0[a].style.opacity = 1;
-          }
-        } this.state = "choosingAvatars";
+
+        switch (this.turn) {
+
+          case 0:
+            if (this.players[0].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "opponent") {
+              this.idArray.forEach((coord) => {
+                if (coord[0] === 1) {
+                  if (this.players[1].characters[coord[1]].isInvulnerable === 0) {
+                    document.getElementById("" + coord[0] + coord[1]).parentNode.style.opacity = 1;
+                  }
+                }
+              })
+            } else if (this.players[0].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "allied") {
+              this.idArray.forEach((coord) => {
+                if (coord[0] === 0) {
+                  if (this.players[0].characters[coord[1]].isInvulnerable === 0 && this.players[0].characters[coord[1]].isActive === 0) {
+                    document.getElementById("" + coord[0] + coord[1]).parentNode.style.opacity = 1;
+                  }
+                }
+              })
+            } else if (this.players[0].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "self") {
+              document.getElementById("" + 0 + this.fightSequenceElement[0]).parentNode.style.opacity = 1;
+            }
+            break;
+
+          case 1:
+            if (this.players[1].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "opponent") {
+              this.idArray.forEach((coord) => {
+                if (coord[0] === 0) {
+                  if (this.players[0].characters[coord[1]].isInvulnerable === 0) {
+                    document.getElementById("" + coord[0] + coord[1]).parentNode.style.opacity = 1;
+                  }
+                }
+              })
+            } else if (this.players[1].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "allied") {
+              this.idArray.forEach((coord) => {
+                if (coord[0] === 1) {
+                  if (this.players[1].characters[coord[1]].isInvulnerable === 0 && this.players[0].characters[coord[1]].isActive === 0) {
+                    document.getElementById("" + coord[0] + coord[1]).parentNode.style.opacity = 1;
+                  }
+                }
+              })
+            } else if (this.players[1].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "self") {
+              document.getElementById("" + 1 + this.fightSequenceElement[0]).parentNode.style.opacity = 1;
+            }
+            break;
+        }
+
+
+        this.state = "choosingAvatars";
         break;
 
       case "avatarChosen":
         this.state = "choosingSkills";
-        for (let i = 0; i <= this.players.length - 1; i++) {
-          for (let j = 0; j <= this.players[i].characters.length - 1; j++) {
-            if (this.players[i].characters[j].chosenSkill !== "") {
-              document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).border = "";
+        console.log(this.players);
+        if (this.turn === 0) {
+          this.idArray.forEach((coord) => {
+            if (coord[0] === 0) {
+              console.log(coord);
+              console.log(this.fightSequenceElement);
+              if (this.players[coord[0]].characters[coord[1]].isActive === 0 && this.players[coord[0]].characters[coord[1]].isInvulnerable === 0 && this.players[coord[0]].characters[coord[1]].chosenSkill === "") {
+                console.log(document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode);
+                document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode.style.opacity = 1;
+              }
             }
-          }
+          })
+        }else if( this.turn === 1){
+          this.idArray.forEach((coord) => {
+            if (coord[0] === 1) {
+              if (this.players[coord[0]].characters[coord[1]].isActive === 0 && this.players[coord[0]].characters[coord[1]].isInvulnerable === 0 && this.players[coord[0]].characters[coord[1]].chosenSkill === "") {
+                document.getElementById("" + coord[0] + coord[1] + coord[2]).parentNode.style.opacity = 1;
+              }
+            }
+          })
         }
-        this.updateDOM();
+        this.fightSequenceElement = [];
         break;
     }
+
+
   }
+  /* selectedSkillsStyling() {
+     //checks for stage and reacts on it with styling changes in regards to selection
+     switch (this.state) {
+       case "choosingSkills":
+         document.getElementsByClassName("gameArea")[0].style.opacity = 1;
+         for (let a = 0; a <= this.avatarTarget.length - 1; a++) {
+           this.avatarTarget[a].style.opacity = 0.7;
+         }
+         //this.avatarTarget.forEach((avatarImg) => {
+         //avatarImg.style.opacity = 0.8;
+         //});
+         for (let i = 0; i <= this.skillNumbers.length - 1; i++) {
+           this.skillNumbers[i].style.opacity = 1;
+         }
+         //this.skillNumbers.forEach((skillImg) => {
+         //skillImg.style.opacity = 1;
+         //});
+         if (this.turn === 0) {
+           for (let i = 0; i <= this.skillsp1.length - 1; i++) {
+             document.getElementsByClassName("p1skills")[i].style.opacity = 0.7;
+           }
+         } else {
+           for (let i = 0; i <= this.skillsp0.length - 1; i++) {
+             document.getElementsByClassName("p0skills")[i].style.opacity = 0.7;
+           }
+         }
+         break;
+ 
+       case "skillChosen":
+         for (let s = 0; s <= this.skillNumbers.length - 1; s++) {
+           this.skillNumbers[s].style.opacity = 0.8;
+         }
+         //this.skillNumbers.forEach((skillImg) => {
+         //skillImg.style.opacity = 0.8;
+         //});
+         for (let i = 0; i <= this.players.length - 1; i++) {
+           for (let j = 0; j <= this.players[i].characters.length - 1; j++) {
+             if (this.players[i].characters[j].chosenSkill !== "") {
+               document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).parentNode.style.opacity = 1;
+               document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).border = "1";
+               //document.getElementById("selectedSkill").src="";
+             }
+           }
+         }
+         //check availability as a valid target
+         if (this.turn === 0 && this.players[0].characters[this.fightSequenceElement[0]].skills[this.fightSequenceElement[1]].viableTarget === "opponent") {
+           for (let a = 0; a <= this.avatarp1.length - 1; a++) {
+             this.players[1].characters.forEach((char) => {
+               if (char.invulnerable !== 0) {
+                 this.avatarp1[a].style.opacity = 1;
+               } else { this.avatarp1[a].style.opacity = 0.7; }
+             })
+           }
+         } else {
+           for (let a = 0; a <= this.avatarp0.length - 1; a++) {
+             this.avatarp0[a].style.opacity = 1;
+           }
+         }
+ 
+         this.state = "choosingAvatars";
+         break;
+ 
+       case "avatarChosen":
+         this.state = "choosingSkills";
+         for (let i = 0; i <= this.players.length - 1; i++) {
+           for (let j = 0; j <= this.players[i].characters.length - 1; j++) {
+             if (this.players[i].characters[j].chosenSkill !== "") {
+               document.getElementById("" + i + j + this.players[i].characters[j].chosenSkill).border = "";
+             }
+           }
+         }
+         this.updateDOM();
+         break;
+     }
+   }*/
 
   addEventhandlerToAllSkills() {
     for (let i = 0; i <= this.skillNumbers.length - 1; i++) {
       this.skillNumbers[i].onclick = () => {
         if (this.state === "choosingSkills") {
           let savedSkill = this.skillNumbers[i].getElementsByTagName("img")[0].getAttribute("id");
+          console.log(savedSkill);
           if (this.players[savedSkill.split("")[0]].characters[savedSkill.split("")[1]].chosenSkill === "") {
             this.players[savedSkill.split("")[0]].characters[savedSkill.split("")[1]].chosenSkill = savedSkill.split("")[2];
+            this.fightSequenceElement = [];
             this.fightSequenceElement.push(savedSkill.split("")[1]);
             this.fightSequenceElement.push(savedSkill.split("")[2]);
             this.state = "skillChosen";
@@ -196,16 +369,16 @@ class IronHackGame {
       this.avatarTarget[i].onclick = () => {
         if (this.state === "choosingAvatars") {
           let savedAvatar = this.avatarTarget[i].getElementsByTagName("img")[0].getAttribute("id");
+          console.log(savedAvatar);
           this.players[savedAvatar.split("")[0]].chosenAvatar = savedAvatar.split("")[1];
-          this.fightSequenceElement.push(this.players[savedAvatar.split("")[0]].chosenAvatar);
+          this.fightSequenceElement.push(savedAvatar.split("")[1]);
           if (this.turn === 0) {
-            this.players[1].characters[parseInt(this.fightSequenceElement[2])].beeingAttacked.push({...this.players[0].characters[parseInt(this.fightSequenceElement[0])].skills[parseInt(this.fightSequenceElement[1])]});
+            this.players[1].characters[parseInt(this.fightSequenceElement[2])].beeingAttacked.push({ ...this.players[0].characters[parseInt(this.fightSequenceElement[0])].skills[parseInt(this.fightSequenceElement[1])] });
           } else {
-            this.players[0].characters[parseInt(this.fightSequenceElement[2])].beeingAttacked.push({...this.players[1].characters[parseInt(this.fightSequenceElement[0])].skills[parseInt(this.fightSequenceElement[1])]});
+            this.players[0].characters[parseInt(this.fightSequenceElement[2])].beeingAttacked.push({ ...this.players[1].characters[parseInt(this.fightSequenceElement[0])].skills[parseInt(this.fightSequenceElement[1])] });
           }
         }
         this.fightSequence.push(this.fightSequenceElement);
-        this.fightSequenceElement = [];
         this.state = "avatarChosen";
         this.updateDOM();
       }
@@ -305,15 +478,19 @@ class Char {
     this.beeingAttacked = [];
     this.beeingBuffed = [];
     this.isInvulnerable = 0;
+    this.playerID = "";
     this.skills = [
-      { type: "damage", value: 10, rounds: 1 },
-      { type: "heal", value: 10, rounds: 1 },
-      { type: "dot", value: 5, rounds: 2 },
-      { type: "invulnerable", value: 1, rounds: 1 }
+      { type: "damage", value: 10, rounds: 1, viableTarget: "opponent", coolDown: 1, isAvailable: 0 },
+      { type: "heal", value: 10, rounds: 1, viableTarget: "opponent", coolDown: 1, isAvailable: 0 },
+      { type: "dot", value: 5, rounds: 2, viableTarget: "allied", coolDown: 2, isAvailable: 0 },
+      { type: "invulnerable", value: 1, rounds: 1, viableTarget: "self", coolDown: 3, isAvailable: 0 }
     ];
   }
 
   recieveDamageAndTypeStateChanges() {
+    if (this.isActive !== 0) {
+      this.isActive -= 1;
+    }
     for (i = 0; i <= this.beeingAttacked.length - 1; i++) {
       switch (type) {
         case "damage":
@@ -328,6 +505,7 @@ class Char {
           this.beeingAttacked = [];
           this.invulnerable = 1;
           this.beeingAttacked.rounds -= 1;
+          this.isActive = 1;
           break;
         case "dot":
           this.health -= value;
@@ -338,8 +516,8 @@ class Char {
           this.isActive = this.beeingAttacked.rounds;
           this.beeingAttacked.rounds -= 1;
       }
-      this.beeingAttacked = this.beeingAttacked.filter((skills) => {return skills.rounds > 0;})
-       
+      this.beeingAttacked = this.beeingAttacked.filter((skills) => { return skills.rounds > 0; })
+
     }
 
   }
